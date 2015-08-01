56b9d41f5552bb634e4be4562c0222a3b3d0f94a
Rebecca Cordingley <rebecca.j.cordingley@gmail.com>
2015-08-01 21:45:27 +0930

    Search widget

---
+++ searchform.php
@@ -0,0 +1,7 @@
+
+    <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+        <label class="screen-reader-text" for="s">SEARCH:</label>
+        <input type="text" value="" placeholder="Search site" name="s" id="s" class="search-widget-input">
+            <button class="btn btn-default search-widget-button" type="submit" id="searchsubmit" style="border: 1px solid #b3b3b3;"><i class="fa fa-arrow-right"></i></button>
+
+    </form>
